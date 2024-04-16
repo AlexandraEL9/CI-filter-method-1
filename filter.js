7f4ab1bedaa539a652342ba3f023b36b756ffdfa
AlexandraEL9 Lee <alexandraelee9@gmail.com>
2024-04-16 12:51:12 +0000

    feat:finished exercises

--- filter.js
+++ filter.js
@@ -20,7 +20,25 @@ const people = [
     age: 18,
   },
 ];
+    //iterate through  array and  create a new array of only those  at least 21
+     //call the filter method on the people 
+     //array and pass it a callback function
+     //use an arrow function that takes the person as a parameter
+     //and returns person.age >=21
+const oldEnough = people.filter(person => person.age >=21);
+     //store result in a variable called oldEnough
+     //log to the console
+console.log(oldEnough); 
+  //returns [ { name: 'Michael', age: 23} ]
 
+     //find objects in an array of objects that contain a specific property
+     // eg just pluck out Paul form the array of people
+  /**
+   * create new constant (paul)
+   * set it to equal people.filter(person => person.name === 'Paul')
+   */
+  const paul = people.filter(person => person.name === 'Paul');
+  console.log(paul); //returns array of Paul's info
 
 // Complex Filtering
 const students = [
@@ -55,3 +73,46 @@ const students = [
     ]
   },
 ];
+   //goal- to create a list of interview 
+   //candidates by filtering out students who don't
+   //have at least 5 years experience in at least
+   //one of the skills
+
+     //filter the students array
+     //pass an arrow func taking the student as parameter
+     //curly braces to contain logic
+     //filter inner skills array to those with at least 5yrs experience
+     //call those strongSkills
+     //for each skill, check if skill.yrsExperience is greater than or equal to 5
+const candidates = students.filter(student => {
+  let strongSkills = student.skills.filter(skill => skill.yrsexperience >= 5);
+  return strongSkills.length > 0;
+});
+console.log(candidates); //returns micheal and paul
+//above code very complex
+
+    //better idea
+    /**
+     * extract whole filter function into
+     * its own function
+     */
+const hasStrongSkills2 = student => {
+  let strongSkills2 = student.skills.filter(skill => skill.yrsexperience >= 5);
+  return strongSkills.length > 0;
+};
+const candidates2 = students.filter(hasStrongSkills);
+//just taken what was inside the parenteses and assigned a variable
+//this isolates the callback and makes it easier to read
+console.log(candidates2);
+
+//better idea
+    /**
+     *could even extract inner filter
+     *to another external function
+     called has5YearsExperience
+     */
+const has5YearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills3 = student => student.skills.filter(has5YearsExp).length > 0;
+const candidates3 = students.filter(hasStrongSkills3);
+console.log(candidates3);
+     
